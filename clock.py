d2b6d96459ea30ab093cd84773c2a20159529054
Dmitro Gierman <mryakzp@gmail.com>
2016-11-07 20:37:19 +0200

    set_digit finished started 'send_value_to_decoder' func

--- clock.py
+++ clock.py
@@ -2,6 +2,12 @@ import datetime
 from time import sleep
 from RPi import GPIO
 
+# инифиализируем гребенку на выход и посылаем туда False (на выходе будет 0 вольт)
+GPIO.setmode(GPIO.BCM)
+for i in range(1, 11):
+    GPIO.setup(i, GPIO.OUT)
+    sleep(0.05)
+    GPIO.output(i, False)
 
 # цифры-разряды наших часов, всего 6 ламп
 digits = [0, 0,  # hour
@@ -10,8 +16,25 @@ digits = [0, 0,  # hour
 
 
 def set_digit(digit, value):
-    """send data to GPIO"""
-    pass
+    """switch on digit through GPIO. Function using only 6 digits: 0-5."""
+    """Appropriate GPIO 1-6"""
+    # нумерация GPIO начинается с 1, а массив digits,
+    # которые их представляют - с 0. Поэтому:
+    digit += 1
+    # вначале потушим лампу, которая сейчас горит:
+    previous_digit = 6 if digit == 1 else digit - 1
+    GPIO.output(previous_digit, False)
+    # теперь зажжем нужную
+    GPIO.output(digit, True)
+    send_value_to_decoder(value)
+
+
+def send_value_to_decoder(value):
+    """кодирует цифру 0-9 в 4-битное число и посылает его на GPIO 7-10"""
+    # 0->7 1->8 2->9 3->10
+
+    bit_view = bin(value + 16)
+    print(bit_view)
 
 # main cycle
 while True:
@@ -31,6 +54,6 @@ while True:
     while datetime.datetime.now() < now + datetime.timedelta(seconds=1):
         print('\r', digits, end='')  # console log
 
-        for digit, value in enumerate(digits):
-            set_digit(digit, value)
+        for key, value in enumerate(digits):
+            set_digit(key, value)
             sleep(0.0008)
